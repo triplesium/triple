764c5dfbf68c9caeeb85e452ad473758a3308741
triplesium <shiss2001@outlook.com>
2025-02-02 23:38:47 +0800

    Last commit probably, migrating to another project

--- modules/ecs_v2/tests/ecs.test.cpp
+++ modules/ecs_v2/tests/ecs.test.cpp
@@ -7,11 +7,14 @@ import triple.base;
 struct Position {
     float x;
     float y;
+
+    auto operator<=>(const Position&) const = default;
 };
 
 struct Velocity {
     float x;
     float y;
+    auto operator<=>(const Velocity&) const = default;
 };
 
 struct Obj {
@@ -19,6 +22,7 @@ struct Obj {
 };
 
 using namespace triple;
+using namespace triple::refl;
 using namespace triple::ecs::v2;
 
 TEST_CASE("Utils", "") {
@@ -35,85 +39,62 @@ TEST_CASE("Utils", "") {
 }
 
 TEST_CASE("Archetype", "") {
-    Archetype<Position, Velocity> a;
     SECTION("Basic") {
-        STATIC_REQUIRE(a.has_component<Position>());
-        STATIC_REQUIRE(a.has_component<Velocity>());
-        STATIC_REQUIRE_FALSE(a.has_component<Obj>());
-
-        auto& x = a.get_column<Position>();
-
-        STATIC_REQUIRE(a.get_column_index<Position>() == 0);
-        STATIC_REQUIRE(a.get_column_index<Velocity>() == 1);
-
-        Entity e = 1;
-        a.add_entity(e, Row {Position {1.f, 2.f}, Velocity {3.f, 4.f}});
-
-        REQUIRE(a.has_entity(e));
-        a.remove_entity(e);
-        REQUIRE_FALSE(a.has_entity(e));
+        Archetype a(0, {type<Position>(), type<Velocity>()});
+        Entity e1 = 0;
+        auto row = a.alloc(e1);
+        a.set_component(type<Position>(), row, Position {1, 2});
+        Position& p = a.get_component(type<Position>(), row).value<Position>();
+        REQUIRE(p.x == 1);
+        REQUIRE(p.y == 2);
     }
-    SECTION("Hash") {
-        Archetype<Position, Velocity> b;
-        Archetype<Velocity, Position> c;
-        Archetype<Position> d;
-        Archetype<Velocity> e;
+}
 
-        STATIC_REQUIRE(a.hash() == b.hash());
-        STATIC_REQUIRE(a.hash() == c.hash());
-        STATIC_REQUIRE(a.hash() != d.hash());
-        STATIC_REQUIRE(a.hash() != e.hash());
-    }
-    SECTION("Slice") {
-        a.add_entity(1, Row {Position {1.f, 2.f}, Velocity {3.f, 4.f}});
-        a.add_entity(2, Row {Position {5.f, 6.f}, Velocity {7.f, 8.f}});
+TEST_CASE("World", "") {
+    World w;
+    SECTION("Components") {
+        Entity e1 = w.entity();
+        Entity e2 = w.entity();
 
-        auto slice = a.slice<Position>();
-        auto iter = slice.begin();
-        auto [pos1] = *iter;
-        REQUIRE(pos1.x == 1.f);
-        REQUIRE(pos1.y == 2.f);
+        w.add_component<Position>(e1, Position {1, 2});
+        w.add_component<Position>(e2, Position {3, 4});
+        w.add_component<Velocity>(e1, Velocity {5, 6});
 
-        ++iter;
-        auto [pos2] = *iter;
-        REQUIRE(pos2.x == 5.f);
-        REQUIRE(pos2.y == 6.f);
+        REQUIRE(w.has_component<Position>(e1));
+        REQUIRE(w.has_component<Position>(e2));
+        REQUIRE(w.has_component<Velocity>(e1));
+        REQUIRE_FALSE(w.has_component<Velocity>(e2));
 
-        ++iter;
-        REQUIRE(iter == slice.end());
+        REQUIRE(w.get_component<Position>(e1).x == 1);
+        REQUIRE(w.get_component<Position>(e1).y == 2);
+        REQUIRE(w.get_component<Position>(e2).x == 3);
+        REQUIRE(w.get_component<Position>(e2).y == 4);
+        REQUIRE(w.get_component<Velocity>(e1).x == 5);
+        REQUIRE(w.get_component<Velocity>(e1).y == 6);
+
+        w.remove_component<Position>(e1);
+        REQUIRE_FALSE(w.has_component<Position>(e1));
     }
 }
 
 TEST_CASE("Query", "") {
-    SECTION("Basic") {
-        Query<Position, Velocity> q1;
-        Query<Position> q2;
+    World w;
+    Entity e1 = w.entity();
+    Entity e2 = w.entity();
 
-        Archetype<Position, Velocity> a1;
-        a1.add_entity(1, Row {Position {1.f, 2.f}, Velocity {3.f, 4.f}});
-        STATIC_REQUIRE(q1.matches(a1));
-        STATIC_REQUIRE(q2.matches(a1));
-
-        Archetype<Position> a2;
-        a2.add_entity(1, Row {Position {5.f, 6.f}});
-        STATIC_REQUIRE_FALSE(q1.matches(a2));
-        STATIC_REQUIRE(q2.matches(a2));
-
-        q2.add_if_matches(a1);
-        q2.add_if_matches(a2);
-
-        auto view = q2.view();
-        auto iter = view.begin();
+    w.add_component<Position>(e1, Position {1, 2});
+    w.add_component<Position>(e2, Position {3, 4});
+    w.add_component<Velocity>(e1, Velocity {5, 6});
+    SECTION("") {
+        Query<Position> q(w);
+        auto iter = q.begin();
         auto [pos1] = *iter;
-        REQUIRE(pos1.x == 1.f);
-        REQUIRE(pos1.y == 2.f);
-
+        REQUIRE((pos1 == Position {1, 2} || pos1 == Position {3, 4}));
         ++iter;
         auto [pos2] = *iter;
-        REQUIRE(pos2.x == 5.f);
-        REQUIRE(pos2.y == 6.f);
-
-        ++iter;
-        REQUIRE(iter == view.end());
+        REQUIRE(
+            ((pos1 != pos2) &&
+             (pos2 == Position {1, 2} || pos2 == Position {3, 4}))
+        );
     }
 }
